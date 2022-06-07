50d981d978868cae387418b655242d9f0604684d
ramaID <dizzy.leaps_0f@icloud.com>
2022-06-07 13:25:01 +0700

    Initial commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+final class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function testTheApplicationReturnsASuccessfulResponse(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
